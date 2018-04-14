9f1fad72f77c02c56f909c177b8f0ea95fd775a4
dvinitsky <daniel.vinitsky@gmail.com>
2018-04-13 21:17:39 -0500

    Placed stock placeholder image

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      
-    
+      {/* change this to a video tag */}
+        <img />
       </div>
     );
   }
